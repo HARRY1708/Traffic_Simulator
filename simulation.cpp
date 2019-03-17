3cb3d1b2cf9f6cc8da117dc5473dd2137ff29bc0
hri98mahesh <33669771+hri98mahesh@users.noreply.github.com>
2019-03-17 14:24:02 +0530

    Update simulation.cpp

--- simulation.cpp
+++ simulation.cpp
@@ -21,29 +21,12 @@ int road_width;
 bool road_signal;
 int default_maxspeed;
 int default_acc;
-int car_length;
-int car_width;
-int car_acc;
-int car_maxspeed;
-int bus_length;
-int bus_width;
-int bus_acc;
-int bus_maxspeed;
-int truck_length;
-int truck_width;
-int truck_acc;
-int truck_maxspeed;
-int bike_length;
-int bike_width;
-int bike_acc;
-int bike_maxspeed;
-int auto_length;
-int auto_width;
-int auto_acc;
-int auto_maxspeed;
-int Simulationtime;
+int Simulationtime=500;
+bool start_simulation;
+bool end_bool;5141
 string current;
 int vehicle_count=-1;
+vector<tuple<string,string,int>> road_vehicle;   //vehicle type,colour,to_wait_for
 tuple <string,int,int,int,int,string> list_of_vehicle[10]; //tuple of <name,length,width,speed,acc,colour>
 class vehicle{  
  public :
@@ -60,8 +43,13 @@ class vehicle{
     this->color=color;
     this->brake=false;
     int xx=0;
-    while(get<0>(list_of_vehicle[xx])!=type)
-          xx++;
+    cout << type;
+    cout << get<0>(list_of_vehicle[0]);
+    while(get<0>(list_of_vehicle[xx])!=type){
+      xx++;
+      cout << xx;
+    }
+          
     
     this->vehicle_length = get<1>(list_of_vehicle[xx]);
     this->vehicle_width = get<2>(list_of_vehicle[xx]);
@@ -89,26 +77,26 @@ class vehicle{
   }
 };
 vector<vehicle> automobiles;
-// void make_frame(){
-//    for(int i=0;i<road_width+2;i++){
-//       for(int j=0;j<road_length;j++){
-//         if(i==0||i==road_width+1)
-//            road[i][j]='-';
-//           else if(!road_signal&&j==50)
-//            road[i][j]='|';
-//           else
-//             road[i][j]=' ';
-//         } 
-//     }
-//     for(int i=0;i<automobiles.size();i++){
-//       automobiles[i].update();
-//       for(int j=automobiles[i].x1;j<=automobiles[i].x2;j++){
-//         for(int k=max(automobiles[i].y2,0);k<=min(automobiles[i].y1,road_length-1);k++){
-//           road[j][k]=automobiles[i].type;
-//         }
-//       }
-//     }
-// }
+void make_frame(){
+   for(int i=0;i<road_width+2;i++){
+      for(int j=0;j<road_length;j++){
+        if(i==0||i==road_width+1)
+           road[i][j]='-';
+          else if(!road_signal&&j==50)
+           road[i][j]='|';
+          else
+            road[i][j]=' ';
+        } 
+    }
+    for(int i=0;i<automobiles.size();i++){
+      automobiles[i].update();
+      for(int j=automobiles[i].x1;j<=automobiles[i].x2;j++){
+        for(int k=max(automobiles[i].y2,0);k<=min(automobiles[i].y1,road_length-1);k++){
+          road[j][k]=automobiles[i].type[0];
+        }
+      }
+    }
+}
 void print_frame(){
    for(int i=0;i<road_width+2;i++){
       for(int j=0;j<road_length;j++){
@@ -127,7 +115,7 @@ void parseLine(const string &line)
   vector<string> tokens;
   for (Tokenizer::iterator iter = tokenizer.begin(); iter != tokenizer.end(); iter++)
     tokens.push_back(*iter);
-  
+  //cout << tokens[0]<< endl;
   if (tokens[0] == "Road_Id")
       Road_Id= stoi(tokens[2]);
   if (tokens[0] == "Road_Length")
@@ -143,7 +131,7 @@ void parseLine(const string &line)
   if(tokens[0]== "Vehicle_Type")
   {
       vehicle_count++;
-      current = tokens[3];
+      current = tokens[2];
       get<0>(list_of_vehicle[vehicle_count]) = current;
       get<3>(list_of_vehicle[vehicle_count]) = default_maxspeed;
       get<4>(list_of_vehicle[vehicle_count]) = default_acc;
@@ -165,6 +153,30 @@ void parseLine(const string &line)
       get<4>(list_of_vehicle[vehicle_count])= stoi(tokens[2]);
 
   }
+  if(tokens[0]=="START")
+  {
+      road_vehicle.push_back(<"start","start",0>)
+  }
+  if(tokens[0]="Signal")
+  {
+    if(tokens[1]="RED"){
+      road_vehicle.push_back(<"red","red",0>);
+    }
+    else
+      road_vehicle.push_back(<"green","green",0>); 
+  }
+  for(int i=0;i<vehicle_count;i++){
+    if(tokens[0]==get<0>(list_of_vehicle[i]))
+    {
+      road_vehicle.push_back(<tokens[0],tokens[1],1>);
+    }
+  }
+  if(tokens[0]="pass"){
+    road_vehicle.push_back(<"pass","pass",15>);
+  }
+  if(tokens[0]="END")
+    end_bool=true;
+
 } 
 
 
@@ -173,7 +185,7 @@ int main(){
     ifstream file;
      file.open("config.ini");
  
-    if (file.is_open())
+    if (file.is_open()){
        while(file.good())
        {
           char line[255];
@@ -181,45 +193,69 @@ int main(){
           string linestring(line);
           parseLine(linestring);
        }
+    }
      else
      {
           cout << "Unable to open the file" << endl;
           return -2;
      }
 
-  //   road_length=100;
-  //   road_width=10;
-  //   road=new char*[road_width];
-  //  for(int i=0;i<road_width+2;i++){
-  //     road[i]=new char[road_length];
-  //     for(int j=0;j<road_length;j++){
-  //       if(i==0||i==road_width+1)
-  //          road[i][j]='-';
-  //         else if(!road_signal&&j==50)
-  //          road[i][j]='|';
-  //         else
-  //           road[i][j]=' ';
-  //       } 
-  //   }
-  //   for(int i=0;i<Simulationtime;i++){
-  //     if(i%50==0)
-  //       road_signal=!road_signal;
-  //     if(i%4==0){
-  //       int x=abs(rand()%5);
-  //           if(x==0)
-  //           automobiles.push_back(vehicle("Truck","Green"));
-  //           else if(x==1)
-  //           automobiles.push_back(vehicle("Car","Green"));
-  //         else if(x==2)
-  //           automobiles.push_back(vehicle("Bus","Green"));
-  //         else if(x==3)
-  //           automobiles.push_back(vehicle("bike","Green"));
-  //         // else 
-  //         //   automobiles.push_back(vehicle("","Green"));
-  //     }
-  //   cout<<"TIME ==>"<<i<<endl;
-  //   make_frame();
-  //   print_frame();
-  //   }
-  // return 0;
+   file.close();
+   // for(int i=0;i<4;i++){
+   //  cout << get<0>(list_of_vehicle[i])<< " " << get<1>(list_of_vehicle[i]) << " " << get<2>(list_of_vehicle[i]) << " " << get<3>(list_of_vehicle[i])<< " " << get<4>(list_of_vehicle[i]) << endl;
+   // }
+   road=new char*[road_width];
+   for(int i=0;i<road_width+2;i++){
+      road[i]=new char[road_length];
+      for(int j=0;j<road_length;j++){
+        if(i==0||i==road_width+1)
+           road[i][j]='-';
+          else if(!road_signal&&j==50)
+           road[i][j]='|';
+          else
+            road[i][j]=' ';
+        } 
+    }
+    tuple<string,string,int> check;
+    check = road_vehicle.pop_front();
+    if(get<0>(check)=="start")
+          start_simulation= true;            
+    if(start_simulation){
+
+            for(int i=0;i<Simulationtime && ;i++){
+                check = road_vehicle.pop_front();
+                if(get<0>(check)=="red")
+                  road_signal=false;
+                else if(get<0>(check)=="green")
+                  road_signal=true;
+                else if(get<0>(check)=="pass")
+                {
+                  if(get<2>(check)==0){
+
+                  }
+                  else{
+                    road_vehicle.push_front(get<0>(check),get<0>(check),get<2>(check)-1);
+                  }
+                }
+                else if(get<0>(check)=="end")
+                {
+                    if(road_empty()){
+                      break;
+                    }
+                    road_vehicle.push_front(get<0>(check),get<0>(check),get<2>(check));
+                }
+                else{
+                  int j;
+                   for(j=0;j<vehicle_count;j++){
+                      if(get<0>(list_of_vehicle[j])=get<0>(check))
+                        automobiles.push_back(vehicle(get<0>(check),get<1>(check)));
+                   }
+                }
+
+              cout<<"TIME ==>"<<i<<endl;
+              make_frame();
+              print_frame();
+              }
+      }
+  return 0;
 }
